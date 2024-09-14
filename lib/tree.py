4afed7424f78afc681c70be0571daf8e2306cb87
kevin <kipronok234@gmail.com>
2024-09-14 16:36:38 +0300

    python-p3-dsa-tree-traversal

--- lib/tree.py
+++ lib/tree.py
@@ -2,5 +2,15 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
-  def get_element_by_id(self, id):
+  def get_element_by_id(self, element_id):
+    def search(node):
+        if node['id'] == element_id:
+            return node
+        for child in node['children']:
+            result = search(child)
+            if result:
+                return result
+        return None
+
+    return search(self.root)  
     pass
